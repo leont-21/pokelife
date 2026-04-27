e8c3024e89a218d8a7af57fbd043a5dee67efabd
BT_C09_24 <mobileappdev@bths.edu>
2026-04-27 09:29:58 -0400

    add pokemon class

---
+++ pokelife/DataStructures.swift
@@ -0,0 +1,16 @@
+//
+//  DataStructures.swift
+//  pokelife
+//
+//  Created by Student on 4/27/26.
+//
+
+import Foundation
+
+
+//TODO add codable
+struct Pokemon : Identifiable {
+    let id : ID
+    let front_default : String
+    let name : String
+}
