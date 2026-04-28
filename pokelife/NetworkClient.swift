2de7e60fae393d3ef595a6f89cc946872d075dc4
BT_C09_21 <mobileappdev@bths.edu>
2026-04-28 08:58:32 -0400

    Added NetworkClient file and getPokemonData function

---
+++ pokelife/NetworkClient.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkClient.swift
+//  pokelife
+//
+//  Created by Student on 4/28/26.
+//
+
+import SwiftUI
+
+struct NetworkClient: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworkClient()
+}
