3bdacfe0f11d64f900cf46289965eecc65bac01d
BT_C09_21 <mobileappdev@bths.edu>
2026-04-28 08:59:04 -0400

    Added NetworkClient file and getPokemonData function

--- pokelife/NetworkClient.swift
+++ pokelife/NetworkClient.swift
@@ -7,12 +7,21 @@
 
 import SwiftUI
 
-struct NetworkClient: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@Observable
+class NetworkClient {
+    func getPokemonData(id: Int) async {
+        let urlStr: String = "https://pokeapi.co/api/v2/pokemon/\(id)"
+        
+        let url: URL? = URL(string: urlStr)
+        guard let urlUnwrapped = url else {
+            return
+        }
+        do {
+            let (data, response) = try await URLSession.shared.data(from: urlUnwrapped)
+            let _ = response as! HTTPURLResponse
+            let pokemon: Pokemon = try JSONDecoder().decode(Pokemon.self, from: data)
+        } catch let error {
+            print(error)
+        }
     }
-}
-
-#Preview {
-    NetworkClient()
 }
