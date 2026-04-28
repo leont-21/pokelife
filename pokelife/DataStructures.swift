cbdbfa2e35b51ce1a2c807826671df1a03f686cc
BT_C09_24 <mobileappdev@bths.edu>
2026-04-28 09:18:04 -0400

    Added shiny + custom decoders to Pokemon class

--- pokelife/DataStructures.swift
+++ pokelife/DataStructures.swift
@@ -8,12 +8,49 @@
 import Foundation
 import SwiftUI
 
-//TODO add codable
-struct Pokemon : Identifiable, Codable {
+struct Pokemon : Identifiable, Decodable {
     let id : Int
-    let front_default : String
+    let sprite_path : String
     let name : String
+    let shiny : Bool
+    
+    //enums for decoding
+    
+    enum CodingKeys : String, CodingKey {
+        case id
+        case name
+        case sprites
+    }
+    
+    enum CodingKeys2 : String, CodingKey {
+        case front_default
+    }
+    
+    //custom decoder, default, sets shiny to false
+    init (from decoder: Decoder) throws {
+        let pokemon = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try pokemon.decode(Int.self, forKey: .id)
+        self.name = try pokemon.decode(String.self, forKey: .sprites)
+        
+        let sprites = try pokemon.nestedContainer(keyedBy: CodingKeys2.self, forKey: .sprites)
+        self.sprite_path = try sprites.decode(String.self, forKey: .front_default)
+        
+        shiny = false
+    }
+    
+    // custom decoder, sets shiny to shiny_s
+    init (from decoder: Decoder, shiny_s : Bool) throws {
+        let pokemon = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try pokemon.decode(Int.self, forKey: .id)
+        self.name = try pokemon.decode(String.self, forKey: .sprites)
+        
+        let sprites = try pokemon.nestedContainer(keyedBy: CodingKeys2.self, forKey: .sprites)
+        self.sprite_path = try sprites.decode(String.self, forKey: .front_default)
+        
+        shiny = shiny_s
+    }
 }
+
 
 struct Task {
     let points : Int //Amount of points gained for one completion of task
