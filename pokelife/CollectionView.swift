733e781abebc1097749d13466acb8d5c62e907a1
BT_C09_24 <mobileappdev@bths.edu>
2026-04-29 09:00:23 -0400

    created collection screen file + added collection to enum

---
+++ pokelife/CollectionView.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionView.swift
+//  pokelife
+//
+//  Created by Student on 4/29/26.
+//
+
+import SwiftUI
+
+struct CollectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CollectionView()
+}
