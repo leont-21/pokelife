d2616eef26a5cabccbbd9f83afbfd93252a13b43
BT_C09_24 <mobileappdev@bths.edu>
2026-04-29 09:25:38 -0400

    Fixed (hopefully) sprites in the datastructs

---
+++ pokelife/PokemonCollectionItem.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonCollectionItem.swift
+//  pokelife
+//
+//  Created by Student on 4/29/26.
+//
+
+import SwiftUI
+
+struct PokemonCollectionItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonCollectionItem()
+}
