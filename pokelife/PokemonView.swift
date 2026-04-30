0b1b0fe35f03a6050eac50c00502a65743cc2f69
BT_C09_21 <mobileappdev@bths.edu>
2026-04-30 09:27:03 -0400

    Added Sprite to PokemonView

--- pokelife/PokemonView.swift
+++ pokelife/PokemonView.swift
@@ -9,18 +9,29 @@ import SwiftUI
 
 struct PokemonView: View {
     @Environment(NetworkClient.self) private var client
-    @State private var pokemon: Pokemon?
+    @State var pokemon: Pokemon
+    let collected: Bool
     
     var body: some View {
-        Text(pokemon?.name ?? "Loading...")
-            .task {
-                await pokemon = client.getPokemonData(id: 5)
+        VStack {
+            AsyncImage(url: URL(string: pokemon.sprites.frontDefault ?? "")) { image in
+                image
+                    .image?.resizable()
+                    .brightness(collected ? 0 : -1)
+                    .frame(width: 125, height: 125)
             }
+            Text(pokemon.name)
+            }
+            .frame(width: 150, height: 150)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.red, lineWidth: 3)
+                )
         }
     }
 
 
 #Preview {
-    PokemonView()
+    PokemonView(pokemon: Pokemon(id: 67, sprite_path: Sprites(frontDefault: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/681.png", frontShiny: "", backDefault: "", backShiny: ""), name: "aegislash shield", shiny:false), collected: false)
         .environment(NetworkClient())
 }
