5dc43ee75a5ef6564055af11912b17643606fcc5
BT_C09_24 <mobileappdev@bths.edu>
2026-04-30 08:56:24 -0400

    working on pokemon collection item

--- pokelife/MainScreen.swift
+++ pokelife/MainScreen.swift
@@ -3,9 +3,9 @@ import SwiftUI
 struct MainScreen: View {
     @Environment(\.dismiss) var dismiss
     @State private var tasks = [
-            "Drink 8 glasses of water",
-            "Take a 10-minute walk"
-            ]
+        "Drink 8 glasses of water",
+        "Take a 10-minute walk"
+    ]
     
     var body: some View {
         VStack (spacing: 8){
@@ -17,27 +17,27 @@ struct MainScreen: View {
                 .font(.system(size: 20, weight: .medium, design: .rounded))
                 .foregroundColor(.white)
                 .shadow(color: .black, radius: 1, x: 0, y: 1)
-            }
-            ScrollView {
-                VStack {
-                    ForEach(tasks, id: \.self) { task in
-                        HStack {
-                            Image(systemName: "star.fill")
-                                .foregroundColor(.yellow)
-                                .font(.system(size: 22))
-                            Text(task)
-                                .font(.system(size: 17, weight: .medium, design: .rounded))
-                                .foregroundColor(.black)
-                            Spacer()
-                            Button(action: {}) {
-                                Text("✓")
-                                //add action here
-                                }
-                            }
-                            .padding()
+        }
+        ScrollView {
+            VStack {
+                ForEach(tasks, id: \.self) { task in
+                    HStack {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                            .font(.system(size: 22))
+                        Text(task)
+                            .font(.system(size: 17, weight: .medium, design: .rounded))
+                            .foregroundColor(.black)
+                        Spacer()
+                        Button(action: {}) {
+                            Text("✓")
+                            //add action here
                         }
                     }
+                    .padding()
                 }
+            }
+        }
         NavigationLink(destination: GachaScreen()) {
             HStack{
                 Image(systemName: "gift.fill")
@@ -53,42 +53,41 @@ struct MainScreen: View {
                     .strokeBorder(Color.black, lineWidth: 3)
             )
         }
+        .padding()
+        
+        Button(action: {
+            dismiss()
+        }) {
+            HStack{
+                Image(systemName: "arrow.left.circle.fill")
+                    .font(.headline)
+                
+                NavigationLink(destination: GachaScreen()) {
+                    Text("Go to Gacha Screen")
+                }
                 .padding()
-                    
-<<<<<<< HEAD
+                
+                NavigationLink(destination: CollectionScreen()) {
+                    Text("Go to Collection Screen")
+                }
+                
                 Button(action: {
                     dismiss()
                 }) {
-                    HStack{
-                        Image(systemName: "arrow.left.circle.fill")
-                            .font(.headline)
-=======
-                    NavigationLink(destination: GachaScreen()) {
-                        Text("Go to Gacha Screen")
-                    }
-                    .padding()
-            
-                    NavigationLink(destination: CollectionScreen()) {
-                        Text("Go to Collection Screen")
-                    }
-            
-                    Button(action: {
-                        dismiss()
-                    }) {
->>>>>>> main
-                        Text("Go Back to Starting Screen")
-                            .font(.system(size: 18, weight: .bold, design: .rounded))
-                    }
-                    .foregroundStyle(.white)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.green)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 25)
-                            .strokeBorder(Color.black, lineWidth: 3)
-                        )
+                    Text("Go Back to Starting Screen")
+                        .font(.system(size: 18, weight: .bold, design: .rounded))
                 }
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity)
+                .background(Color.green)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 25)
+                        .strokeBorder(Color.black, lineWidth: 3)
+                )
+            }
             .padding()
-        .navigationBarHidden(true)
+            .navigationBarHidden(true)
+        }
     }
 }
 
--- pokelife/PokemonCollectionItem.swift
+++ pokelife/PokemonCollectionItem.swift
@@ -14,10 +14,13 @@ struct PokemonCollectionItem: View {
     var pokemon : Pokemon
     var body: some View {
         VStack {
-            AsyncImage(url: URL(string: pokemon.sprite_path))
-                .frame(width: 140, height: 130)
-                .aspectRatio(contentMode: .fill)
-                .background(.green)
+            if let urlString : String = pokemon.sprites["front_default"] {
+                
+                AsyncImage( )
+                    .frame(width: 140, height: 130)
+                    .aspectRatio(contentMode: .fill)
+                    .background(.green)
+            }
         }
         .frame(width: 150, height: 200)
         .background(.black)
@@ -25,5 +28,5 @@ struct PokemonCollectionItem: View {
 }
 
 #Preview {
-    PokemonCollectionItem(pokemon: Pokemon(id: 67, sprite_path: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/681.png", name: "aegislash shield", shiny:false))
+    PokemonCollectionItem(pokemon: Pokemon(id: 67, sprites: ["front_default" : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/681.png"] , name: "aegislash shield", shiny:false))
 }
