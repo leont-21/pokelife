d9cc5a82dc1cab99c57f9434ef3275bef372f07e
BT_C09_21 <mobileappdev@bths.edu>
2026-05-06 09:28:13 -0400

    Added netowrkclient environment

--- pokelife/pokelifeApp.swift
+++ pokelife/pokelifeApp.swift
@@ -4,11 +4,13 @@ import SwiftUI
 @main
 struct pokelifeApp: App {
     @State private var gameModel = GameModel()
+    @State private var networkClient = NetworkClient()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(gameModel)
+                .environment(networkClient)
         }
     }
 }
