c0b85abaf887692236cf03fdff576cd4c74d736f
BT_C09_27 <mobileappdev@bths.edu>
2026-05-08 09:32:36 -0400

    worked on GachaGame gui & knob!

--- pokelife/GachaGame.swift
+++ pokelife/GachaGame.swift
@@ -3,22 +3,26 @@ import SwiftUI
 
 struct GachaGame: View {
     @Environment(GameModel.self) private var model
+    
+    // knob turning for gacha!
     @State private var gachaOn = false
     @State private var rotation: Angle = .zero
-    
     @State private var arrowVis = true
     @State private var totalRotation: Int = 0
     
     var body: some View {
         ZStack {
             // ADD BARS TO TOP & BOTTOM FOR VISUAL EFFECT
-            Rectangle()
-                .foregroundColor(.gray)
-                .frame(width: 250, height: 50)
             Color(red: 1.0, green: 0.85, blue: 0.9)
                 .ignoresSafeArea()
             ZStack() {
+                Rectangle()
+                    .foregroundColor(.brown)
+                    .frame(width: 300, height: 105)
+                    .offset(y: -275)
+                
                 Image("gachapon")
+                
                 if !gachaOn {
                     Image("arrow")
                         .opacity(arrowVis ? 1 : 0.7)
@@ -34,6 +38,7 @@ struct GachaGame: View {
                         .offset(x: 7, y: 57)
                         .rotationEffect(.degrees(-26))
                 }
+                
                 VStack {
                     Image("gachapon_knob")
                         .rotationEffect(rotation, anchor: .center)
@@ -46,6 +51,11 @@ struct GachaGame: View {
                         )
                 }
                 .offset(x: 50, y: 100)
+                
+                Rectangle()
+                    .foregroundColor(.brown)
+                    .frame(width: 300, height: 80)
+                    .offset(y: 250)
             }
             .scaleEffect(1.5)
         }
@@ -55,14 +65,26 @@ struct GachaGame: View {
     func rotateKnob(position: DragGesture.Value) -> Angle {
         let vector = CGVector(dx: position.location.x - 50, dy: position.location.y - 110)
         let radians = atan2(vector.dy, vector.dx)
-        let angle = Angle(radians: Double(radians))
+        let newAngle = Angle(radians: Double(radians))
+        var delta = newAngle.degrees - rotation.degrees
         
-        // MAKE this activate something!!
-        if totalRotation <= 1080 {
-            totalRotation += Int(angle.degrees)
+        if delta <= -180 { delta += 360 }
+        if delta > 180 { delta -= 360 }
+        
+        // check if movement is clockwise
+        if delta > 0 {
+            if totalRotation < 1080 {
+                totalRotation += Int(delta)
+                
+                if totalRotation >= 1080 {
+                    // give the player a pokemon!!
+                }
+            }
+            return newAngle
+        } else {
+            // go back to previous position if countercw
+            return rotation
         }
-        
-        return angle
     }
 }
 
